8c69220c4cfce000f96bddeef26af4c2ff81b117
Chi Pham <chigau02@gmail.com>
2019-05-02 20:21:15 +0700

    Assignemt 4

---
+++ T1806E - CSharp/Assignment 4/Menu.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1806E___CSharp.Assignment_4
+{
+    public class Menu
+    {
+        public static void Main(String[] args)
+        {
+            List<News> ListNews = new List<News>();
+            bool start = true;
+            while(start)
+            {
+                Console.WriteLine("Menu: ");
+                Console.WriteLine("1. Insert news");
+                Console.WriteLine("2. View list news");
+                Console.WriteLine("3. Average rate");
+                Console.WriteLine("4. Exit");
+
+                int menu = Int16.Parse(Console.ReadLine());
+                if(menu > 0 || menu < 5)
+                {
+                    switch (menu)
+                    {
+                        case 1:
+                            ListNews.Add(new News());
+                            break;
+                        case 2:
+                            ViewAll(ListNews); 
+                            break;
+                        case 3:
+                            AvgAndViewAll(ListNews);
+                            break;
+                        case 4:
+                            start = false;
+                            break;
+                    }
+                    
+                }
+                    
+            } 
+        }
+
+        public static void ViewAll(List<News> listNews)
+        {
+            foreach(News x in listNews)
+            {
+                x.Display();
+            }
+        }
+
+        public static void AvgAndViewAll(List<News> listNews)
+        {
+            foreach(News x in listNews)
+            {
+                x.Calculate();
+                x.Display();
+            }
+        }
+    }
+}
---
+++ T1806E - CSharp/Assignment 4/News.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1806E___CSharp.Assignment_4
+{
+    public class News : INews
+    {
+        public int ID;
+        public String Title;
+        public String PublishDate;
+        public String Author;
+        public String Content;
+        public float AverageRate;
+        public int[] RateList = new int[3];
+
+
+        public News()
+        {
+            Console.WriteLine("Nhập ID: ");
+            ID = Int32.Parse(Console.ReadLine());
+            Console.ReadLine();
+            Console.WriteLine("Nhập Title: ");
+            Title = Console.ReadLine();
+            Console.WriteLine("Nhập PublishDate: ");
+            PublishDate = Console.ReadLine();
+            Console.WriteLine("Nhập Content: ");
+            Content = Console.ReadLine();
+
+            Console.WriteLine("3 rate: ");
+            for(int i=0; i<3; i++)
+            {
+                RateList[i] = Int16.Parse(Console.ReadLine());
+            }
+
+        }
+        
+        public int id
+        {
+            get
+            {
+                return ID;
+            }
+            set
+            {
+                this.id = value;
+            }
+        }
+
+        public String title
+        {
+            get
+            {
+                return Title;
+            }
+            set
+            {
+                this.Title = value;
+            }
+        }
+
+        public String publishDate
+        {
+            get
+            {
+                return PublishDate;
+            }
+            set
+            {
+                this.PublishDate = value;
+            }
+        }
+
+        public String author
+        {
+            get
+            {
+                return Author;
+            }
+            set
+            {
+                this.Author = value;
+            }
+        }
+
+        public String content
+        {
+            get
+            {
+                return Content;
+            }
+            set
+            {
+                this.Content = value;
+            }
+        }
+
+
+        public void Display()
+        {
+            Console.WriteLine("ID: " + ID + "Title: " + Title + "PublishDate: " + PublishDate + "Author: " + Author + "Content: " + Content +"AverageRate: " + AverageRate);
+        }
+
+        public void Calculate()
+        {
+            int total = 0;
+            foreach(int i in RateList)
+            {
+                total += i;
+            }
+            AverageRate = (float)total / 3;
+        }
+    }
+}
