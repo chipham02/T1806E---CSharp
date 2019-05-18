d619c6d26a00b1b79d9ab9f42ae1422b6eaad732
Chi Pham <chigau02@gmail.com>
2019-05-18 20:28:09 +0700

    CSharp

--- T1806E - CSharp/Session 7/Program.cs
+++ T1806E - CSharp/Session 7/Program.cs
@@ -9,9 +9,45 @@ namespace T1806E___CSharp.Session_7
     class Program
     {
         public delegate void showString(String s);
+        
+        public class EventTest
+        {
+            public event showString Click;
+            public int age = 0;
+            public void showEvent()
+            {
+                if(Click != null)
+                {
+                    Click("Click here...");
+                }
+            }
+
+            public int Age
+            {
+                get
+                {
+                    return age;
+                }
+                set
+                {
+                    if(this.age !=0 && this.age != value)
+                    {
+                        Click("Age was changed");
+                    }
+                    this.Age = value;
+                }
+            }
+        }
 
         public static void Main(string[] args)
         {
+            EventTest ev = new EventTest();
+            ev.Click += showStringProgram;
+
+            ev.showEvent();
+            ev.Age = 20;
+            ev.Age = 22;
+
             showString ss = new showString(showStringProgram);
             showString s2 = new showString(showStringPro);
             ss += s2;
